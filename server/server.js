a7a427431784c600478902dd2b1493cb604aacc1
eshinx1 <eshinx1@gmail.com>
2016-04-02 12:21:02 -0700

    Tweet displayed in console - removed keys to a gitignored file

--- server/server.js
+++ server/server.js
@@ -1,13 +1,42 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var twitNode = require('twitter-node-client');
+var Twitter = require('twitter-node-client').Twitter;
+// var config = require('./config.js')
 
 var app = express();
 
+var config = {
+
+
+}
+
+var twitter = new Twitter(config);
+
+
+
+
+var error = function (err, response, body) {
+    console.log('ERROR [%s]', err);
+};
+var success = function (data) {
+    dataObj = JSON.parse(data);
+    console.log("Success");
+    console.log('TWEET of the day', dataObj[0].text);
+};
+
+
+
+
+twitter.getUserTimeline({ screen_name: 'eshinx1', count: '10'}, error, success);
 
 app.get("/", function(req, res){
   res.send("hello")
-})
+});
+
+
+
+
+
 
 
 
