19a287405794c3a800bf5a0f52fb05d1a4302aa1
eshinx1 <eshinx1@gmail.com>
2016-04-02 17:15:51 -0700

    Tweets Posting, Username input is almost complete

--- client/app.js
+++ client/app.js
@@ -2,5 +2,64 @@ var app = {
 
 
 
+  init : function(){
+    $( "#target" ).on("submit", app.postUsername);
 
-};
+    app.getTweets();
+    // app.postTweets();
+  },
+
+  postUsername : function(){
+    var newUsername = $("#username").val();
+
+    console.log("Username is ", newUsername);
+    $.ajax({
+        type: 'post',
+        url: 'http://localhost:8080/',
+        data: newUsername,
+        contentType: "text/plain",
+        success: function(data) {
+
+            console.log('success');
+            console.log("this is data", data);
+            // displayTweets();
+        },
+        error: function(){
+          console.log("error")
+        }
+      })
+
+
+  },
+
+  getTweets : function(){
+    console.log("getTweets ran");
+    $.ajax({
+        type: 'get',
+        url: 'http://localhost:8080/',
+        success: function(data) {
+            console.log('success');
+            console.log(data);
+            app.displayTweets(data);
+        },
+        error: function(){
+          console.log("error")
+        }
+      })
+
+
+
+  },
+
+  displayTweets: function(data){
+
+    for(var i = 0; i < data.length; i++){
+      $("#tweets").append("<p>" + " : " + data[i] +"</p>");
+  }
+
+
+
+
+
+}
+}
--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var Twitter = require('twitter-node-client').Twitter;
+var cors = require('cors');
 // var config = require('./config.js')
 
 var app = express();
 
-var config = {
 
+
+
+
+// var headers = {
+//   "access-control-allow-origin" : "*",
+//   "access-control-allow-methods" : "GET, POST",
+//   "access-control-allow-headers": "content-type",
+// };
+
+var config = {
+    "consumerKey": "40HUjXzPr2vyhLOEnjsm7Nlxm",
+    "consumerSecret": "31LQt4A00pqcorp6zdBvcWJIRX8t6o73EHlSNidQy1qQUJJnT7",
+    "accessToken": "3255064730-Yo8w7fBj0rvecPR9ipgF5UflEyC8ts0Ta0VtG9b",
+    "accessTokenSecret": "lIKiCIKVlOYXBWJ1l8Bp6VeatzdnjvN8MLIvRadSkIXZz"
 
 }
 
@@ -19,21 +33,57 @@ var error = function (err, response, body) {
     console.log('ERROR [%s]', err);
 };
 var success = function (data) {
-    dataObj = JSON.parse(data);
     console.log("Success");
-    console.log('TWEET of the day', dataObj[0].text);
+    console.log('data', data);
 };
 
+app.use(bodyParser());
+
+app.use(bodyParser.json());
 
 
-
-twitter.getUserTimeline({ screen_name: 'eshinx1', count: '10'}, error, success);
-
-app.get("/", function(req, res){
-  res.send("hello")
+app.use(function(req, res, next) {
+  res.header("access-control-allow-origin", "*");
+  res.header("access-control-allow-methods", "GET, POST")
+  res.header("access-control-allow-headers", "Content-Type");
+  next();
 });
 
+var tweets = [];
+var name = "adidas";
+var username = { screen_name: name, count: '30'};
+var unArray = [];
 
+twitter.getUserTimeline(username, error, function(data) {
+  dataObj = JSON.parse(data);
+  console.log(success(dataObj))
+  for(var i = 0; i < dataObj.length; i++) {
+  tweets.push(dataObj[i].text);
+  // unArray.push(dataObj[i].user.name)
+
+  console.log(tweets);
+}
+});
+
+app.get("/", function(req, res){
+  // res.writeHead(200, headers);
+  res.send(tweets);
+
+
+  // res.render(tweets);
+  console.log("hello");
+});
+
+app.post("/", function(req, res) {
+  console.log(req.body.username);
+  // res.send('You sent the name "' + req.body.name + '".');
+  // res.end(tweets);
+
+  // unArray.push(req.body.username);
+  console.log("DId this work?");
+  console.log(unArray);
+
+});
 
 
 
